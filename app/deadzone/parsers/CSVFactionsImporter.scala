934b5b744a95eb1a9f5356b829166e54fbea7266
Sebastian Hardt <tuxburner@boggo.de>
2018-02-12 23:21:23 +0100

    string template

--- app/deadzone/parsers/CSVFactionsImporter.scala
+++ app/deadzone/parsers/CSVFactionsImporter.scala
@@ -63,35 +63,35 @@ class CSVFactionsImporter @Inject()(configuration: Configuration, csvWeaponImpor
   def getSoldierForFaction(factionName: String) = soldiers.filter(_.faction == factionName)
 
   private def importSoldiersFromCsvs(): List[CSVTroopDto] = {
-    val dataWithHeaders =  readCsvFile("deadzone/armies.csv")
+    val dataWithHeaders = readCsvFile("deadzone/armies.csv")
     dataWithHeaders.map(parseLineMap(_)).flatten
   }
 
   private def parseLineMap(lineData: Map[String, String]): Option[CSVTroopDto] = {
 
-    val faction =  lineData.get(FACTION_HEADER).get
+    val faction = lineData.get(FACTION_HEADER).get
     if (faction.isEmpty == true) {
-      Logger.error("CSV Troop: No faction given in line: " + lineData)
+      Logger.error(s"CSV Troop: No faction given in line: ${lineData}")
       return Option.empty
     }
 
     val name = lineData.get(NAME_HEADER).get
     if (name.isEmpty == true) {
-      Logger.error("CSV Troop: No name given for soldier for faction: " + faction + " in line: " + lineData)
+      Logger.error(s"CSV Troop: No name given for soldier for faction: ${faction} in line: ${lineData}")
       return Option.empty
     }
 
 
     val pointsData = lineData.get(POINTS_HEADER).get
     if (pointsData.isEmpty == true) {
-      Logger.error("CSV Troop: No point given for soldier: " + name + " for faction: " + faction + " in line: " + lineData)
+      Logger.error(s"CSV Troop: No point given for soldier: ${name} for faction: ${faction} in line: ${lineData}")
       return Option.empty
     }
     var points = pointsData.toInt
 
     val typeData = lineData.get(TYPE_HEADER).get
     if (typeData.isEmpty == true) {
-      Logger.error("CSV Troop: No type given for soldier: " + name + " for faction: " + faction + " in line: " + lineData)
+      Logger.error(s"CSV Troop: No type given for soldier: ${name} for faction: ${faction} in line: ${lineData}")
       return Option.empty
     }
 
@@ -104,7 +104,7 @@ class CSVFactionsImporter @Inject()(configuration: Configuration, csvWeaponImpor
       case _ => None
     }
     if (matchedTyp == None) {
-      Logger.error("No type matched for: " + typeData + " for soldier: " + name + " for faction: " + faction + " in line: " + lineData)
+      Logger.error(s"No type matched for: ${typeData} for soldier: ${name} for faction: ${faction} in line: ${lineData}")
       return Option.empty
     }
 
@@ -129,7 +129,7 @@ class CSVFactionsImporter @Inject()(configuration: Configuration, csvWeaponImpor
     val armour = lineData.get(ARMOUR_HEADER).get.trim.toInt
     var victoryPoints = lineData.get(VICTORY_POINTS_HEADER).get.trim.toInt
 
-    val hardPoints = csvWeaponImporter.getNumberWithDefault(lineData.get(HARDPOINTS_HEADER),0)
+    val hardPoints = csvWeaponImporter.getNumberWithDefault(lineData.get(HARDPOINTS_HEADER), 0)
 
     val abilitiesData = lineData.get(ABILITIES_HEADER).get.trim
     val abilities = csvWeaponImporter.parseAbilities(abilitiesData)
@@ -150,7 +150,7 @@ class CSVFactionsImporter @Inject()(configuration: Configuration, csvWeaponImpor
             defaultWeaponNames += matchedWeapon.name
           })
           .getOrElse(
-            Logger.error("Weapon: " + trimmedWeaponName + " not found for troop: " + name + " faction: " + faction)
+            Logger.error(s"Weapon: ${trimmedWeaponName} not found for troop: ${name} faction: ${faction}")
           )
       })
     }
@@ -160,7 +160,7 @@ class CSVFactionsImporter @Inject()(configuration: Configuration, csvWeaponImpor
 
     val imgUrl = lineData.get(IMG_URL_HEADER).getOrElse("")
 
-    Option.apply(CSVTroopDto(faction, name, points, matchedTyp.asInstanceOf[ModelType.Value], speed, shootRange, fightValue, surviveValue, sizeValue, armour, victoryPoints, abilities, defaultWeaponNames.toList,weaponTypes,hardPoints,recon,armySpecial,items,imgUrl))
+    Option.apply(CSVTroopDto(faction, name, points, matchedTyp.asInstanceOf[ModelType.Value], speed, shootRange, fightValue, surviveValue, sizeValue, armour, victoryPoints, abilities, defaultWeaponNames.toList, weaponTypes, hardPoints, recon, armySpecial, items, imgUrl))
   }
 
   private def parsePlusValue(data: String): Int = {
