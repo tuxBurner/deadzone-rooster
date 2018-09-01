aa5e27b297b9619370139aa4526125a791a21bd9
Sebastian Hardt <tuxburner@boggo.de>
2018-09-01 14:09:20 +0200

    Specialist special description added

---
+++ app/controllers/FixMessagesKey.scala
@@ -0,0 +1,14 @@
+package controllers
+
+object FixMessagesKey {
+
+  def fixKey(keyToFix: String): String = {
+     keyToFix
+       .toLowerCase
+       .replace("ü", "ue")
+       .replace("ä", "ae")
+       .replace("ö", "oe")
+       .replace("ß", "ss")
+  }
+
+}
